8421d00520e4cf234f2f4af629d5245b38c95539
Constantinos-Barmpouris <barmpourisc@ufl.edu>
2025-11-23 22:00:09 -0500

    cvrp all csv files

--- CVRP_Optimal/main.jl
+++ CVRP_Optimal/main.jl
@@ -20,26 +20,28 @@ using .SolIO
 using .SolverSCF
 using .BatchCVRP
 
+base = @__DIR__
+
 # 1) Solve a single instance as a smoke test
 # Use the script folder as the anchor so it works no matter your working directory.
-base = @__DIR__
-vrp_path = joinpath(base, "A", "A-n32-k5.vrp")
-inst = VRPLIB.parse_vrplib(vrp_path)
-K = SolIO.parse_K_from_filename(vrp_path)
-res = SolverSCF.solve_cvrp_scf(inst; K=K, timelimit=10.0*inst.n, mipgap=0.0, seed=0)
+# vrp_path = joinpath(base, "A", "A-n32-k5.vrp")
+# inst = VRPLIB.parse_vrplib(vrp_path)
+# K = SolIO.parse_K_from_filename(vrp_path)
+# res = SolverSCF.solve_cvrp_scf(inst; K=K, timelimit=2.0*inst.n, mipgap=0.0, seed=0)
 
-println("Single instance: ", vrp_path)
-println("  objective = ", res.objective,
-        " | gap = ", res.mip_gap,
-        " | runtime = ", res.runtime_sec,
-        " | vehicles = ", res.vehicles)
-
-routes = VRPUtils.extract_routes_from_x(res.xvals, inst.depot)
-VRPUtils.pretty_print_routes(routes, inst)
+# println("Single instance: ", vrp_path)
+# println("  objective = ", res.objective,
+#        " | gap = ", res.mip_gap,
+#        " | runtime = ", res.runtime_sec,
+#        " | vehicles = ", res.vehicles)
+#
+#routes = VRPUtils.extract_routes_from_x(res.xvals, inst.depot)
+#VRPUtils.pretty_print_routes(routes, inst)
 
 # 2) Batch all instances in folder "A" and write CSV
 folder = joinpath(base, "A")
-rows = BatchCVRP.batch_solve(folder; time_coeff=10.0, seed=0)
-outcsv = joinpath(folder, "batch_results.csv")
+rows = BatchCVRP.batch_solve(folder; time_coeff=2.0, seed=0)
+outcsv = joinpath(folder, "batch_results_2n_0.25h.csv")
 BatchCVRP.write_results_csv(outcsv, rows)
+BatchCVRP.print_results(rows)
 println("\nBatch summary written to: ", outcsv)
